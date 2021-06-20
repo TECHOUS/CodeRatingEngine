4ea307593d644864d5100aec016af34cd6a4de84
GauravWalia19 <gauravwalia019@gmail.com>
2021-06-20 22:23:09 +0530

    completed the job

--- .github/job.js
+++ .github/job.js
@@ -1,30 +1,48 @@
-const {exec} = require('child_process');
+const { default: axios } = require('axios');
+const { exec } = require('child_process');
+const codeRatingModel = require('../models/codeRatingModel');
+const mongoose = require('mongoose');
 
-exec('echo "MONGODB_URI=mongodb+srv://techous:EH0bCYSBk6pk0PMD@initialcluster.ng9ou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" > .env', (err, stdout, stderr)=>{
-    if(err){
-        return;
-    }else{
-        console.log(`stdout: ${stdout}`);
-        const MongoClient = require('mongodb').MongoClient;
-        require('dotenv').config()
+const connectMongoDB = () => {
+    require('dotenv').config()
+    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true});
+    const db = mongoose.connection;
+    db.on('error',console.error.bind(console, 'Connection error:'));
+    return db;
+}
 
-        const uri = process.env.MONGODB_URI;
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log(client);
-    }
-})
+// this function handles calling the API
+const callGithubCodeBaseAPI = () => {
+    axios.get('https://api.github.com/repos/TechOUs/HacktoberFest21Community/contents/CodeBase')
+        .then(res => {
+            const db = connectMongoDB()
+            const values = res.data.map((codeObject) => {
+                return {
+                    codeId: codeObject.sha,
+                    codeName: codeObject.name,
+                    codeUrl: codeObject.url,
+                    codeRating: 0
+                };
+            })
+            // console.log(values);
+            codeRatingModel.insertMany(values)
+            .then(()=>{
+                console.log("Data added successfully")
+                db.close();
+            })
+            .catch((err) => {
+                console.log("Data is not added: "+err);
+                db.close();
+            })
+        })
+        .catch(err => console.log(err))
+}
 
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
-
-exec('ls', (err, stdout, stderr)=>{
-    if(err){
+// create the .env file for linking
+exec('echo "MONGODB_URI=mongodb+srv://techous:EH0bCYSBk6pk0PMD@initialcluster.ng9ou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" > .env', (err, stdout, stderr) => {
+    if (err) {
         console.log(err);
-    }
-    else{
-        console.log(`ls: ${stdout}`)
+    } else {
+        callGithubCodeBaseAPI()
     }
 })
---
+++ models/codeRatingModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const codeRatingModel = new Schema({
+    codeId: {
+        type: String,
+        unique: true
+    },
+    codeName: {
+        type: String,
+        unique: true
+    },
+    codeRating:{
+        type: Number
+    },
+    codeUrl: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('codeRating',codeRatingModel);
