7a50dc4dd9c8ab1cd5a04f93473a7932c6901059
GauravWalia19 <gauravwalia019@gmail.com>
2021-07-03 23:16:45 +0530

    added the rate limiting 10 requests per minute for this API and updated node engine version to 14

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require("express-rate-limit");
 
 const app = express();
+
+const rateLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,    // 1 minute
+    max: 10,
+    message: {
+        status: 429,
+        error: "Too many requests, please try again later!!!"
+    }
+});
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+// rate limiting middleware
+app.use(rateLimiter);
 
 // exclusing dotenv config from production
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
