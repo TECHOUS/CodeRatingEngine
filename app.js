df9bee30963dd5dbf7471d6f87512c29cf9c6b14
Gaurav Walia <gauravwalia019@gmail.com>
2021-07-24 19:36:18 +0530

    Added the vercel deployment configurations (#1)
    * Updated folder structure and updated test suite
    
    * added the vercel configurations
    
    * deployed in development mode

--- app.js
+++ app.js
@@ -36,11 +36,4 @@ app.use(express.urlencoded({extended: false}));
 // middleware for handling sample api routes
 app.use('/api/v1', require('./routes/api/API'));
 
-// use port from environment variables for production
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`);
-})
-
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,40 +1,52 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../app');
 
 describe('testing the code rating engine APIs',() => {
     let token = "";
+    let codeId1 = "";
+    let codeId2 = "";
+    const SEARCH_USER = "GauravWalia19"; 
     beforeEach('get the random codes with token', async () => {
         try{
             const res = await request(app)
             .get('/api/v1/randomCodes')
             .expect(200)
             token = res.body.accessToken;
+            codeId1 = res.body.codeObject1.codeId;
+            codeId2 = res.body.codeObject2.codeId;
         }catch(err){
             console.log(err)
         }
     });
     
-    it('testing winner calculations', ()=>{
+    it('testing winner calculations', (done)=>{
         request(app)
         .put('/api/v1/rateCode')
         .type('json')
         .send({
             'winner': 1,
-            'codeId1': '55dfc162f8163243c9c7e155e9a9a26b9528be73',
-            'codeId2': '489e8fe91b2db73cc0f8f78b831fabb1b927d2b5',
+            codeId1,
+            codeId2,
             'codeRatingEngineToken': token
         })
         .expect(200)
+        .end((err, res)=>{
+            if(err) done(err)
+            else
+                done(); 
+        })
     })
 
-    it('search the users', ()=>{
+    it('search the users', (done)=>{
         request(app)
         .get('/api/v1/searchUser')
-        .query({username: 'GauravWalia19'})
+        .query({username: SEARCH_USER})
         .send({'codeRatingEngineToken': token})
         .expect(200)
         .end((err, res)=>{
-            if(err) console.log(err)
+            if(err) done(err)
+            else
+                done(); 
         })
     })
 });
