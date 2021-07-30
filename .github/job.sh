0c0e6399d0529eff3dec50618d4dbcb1a43c5430
Gaurav Walia <gauravwalia019@gmail.com>
2021-07-30 23:49:05 +0530

    Cleaned the Keys from the job (#3)
    * Updated folder structure and updated test suite
    
    * added the vercel configurations
    
    * deployed in development mode

--- .github/job.sh
+++ .github/job.sh
@@ -1,13 +1,7 @@
 # /bin/bash
 # this shell script it to read all the code from the community project and
-# upload to mongodb 
-# run this script locally only from root
+# upload to mongodb run this script locally only from root
 # bash .github/job.sh
-
-# echo "MONGODB_URI=mongodb+srv://techous:EH0bCYSBk6pk0PMD@initialcluster.ng9ou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" > .env
-# echo "RATE_LIMIT_MINUTES=1" >> .env
-# echo "RATE_LIMIT_MAX_REQUEST=10" >> .env
-# echo "CACHE_STORAGE_SECONDS=30" >> .env
 
 git clone "https://github.com/TechOUs/HacktoberFest21Community.git"
 
@@ -15,6 +9,5 @@ node .github/job.js
 
 # cleanup
 echo "Cleanup..."
-rm .env
 rm -rf HacktoberFest21Community
 echo "Cleanup...Done"
